1ffe0fa1828f11c3e20dfe982240abd83f74f1a2
Ivan Tkachenko <vanekt@yandex>
2016-08-30 20:21:13 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/frontend/app.jsx',
+    output: {
+        path: './build' ,
+        filename: 'app.bundle.js'
+    },
+    devtool: 'source-map',
+    debug: true,
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/frontend/index.html'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                include: './src/frontend/',
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
